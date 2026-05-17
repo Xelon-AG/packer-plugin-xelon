722a5fd7e2e10fd6e7eb512344f0af9b7584ea64
pavel <36902936+pavel-github@users.noreply.github.com>
2026-05-17 20:13:33 +0200

    refactor: add validation for AccessConfig

--- .web-docs/README.md
+++ .web-docs/README.md
@@ -2,8 +2,8 @@ The [Xelon](https://www.xelon.ch/) Packer plugin provides a builder for building
 
 ### Installation
 
-To install this plugin, copy and paste this code into your Packer configuration, then run [
-`packer init`](https://www.packer.io/docs/commands/init).
+To install this plugin, copy and paste this code into your Packer configuration, then run
+[`packer init`](https://www.packer.io/docs/commands/init).
 
 ```hcl
 packer {
@@ -29,20 +29,7 @@ The Scaffolding plugin is intended as a starting point for creating Packer plugi
 #### Builders
 
 - [builder](/packer/integrations/hashicorp/scaffolding/latest/components/builder/builder-name) - The scaffolding builder
-  is used to create endless Packer
-  plugins using a consistent plugin structure.
-
-#### Provisioners
-
-- [provisioner](/packer/integrations/hashicorp/scaffolding/latest/components/provisioner/provisioner-name) - The
-  scaffolding provisioner is used to provisioner
-  Packer builds.
-
-#### Post-processors
-
-- [post-processor](/packer/integrations/hashicorp/scaffolding/latest/components/post-processor/postprocessor-name) - The
-  scaffolding post-processor is used to
-  export scaffolding builds.
+  is used to create endless Packer plugins using a consistent plugin structure.
 
 #### Data Sources
 
--- internal/builder/config_test.go
+++ internal/builder/config_test.go
@@ -9,6 +9,89 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestAccessConfig_Prepare(t *testing.T) {
+	type testCase struct {
+		input          *AccessConfig
+		expectedConfig AccessConfig
+		expectedErrors int
+	}
+	tests := map[string]testCase{
+		"required values": {
+			input: &AccessConfig{
+				ClientID: "client-id-value",
+				Token:    "token-value",
+			},
+			expectedConfig: AccessConfig{
+				ClientID: "client-id-value",
+				Token:    "token-value",
+			},
+		},
+		"missing client_id": {
+			input: &AccessConfig{
+				Token: "token-value",
+			},
+			expectedConfig: AccessConfig{},
+			expectedErrors: 1,
+		},
+		"missing token": {
+			input: &AccessConfig{
+				ClientID: "client-id-value",
+			},
+			expectedConfig: AccessConfig{},
+			expectedErrors: 1,
+		},
+		"missing client_id and token": {
+			input:          &AccessConfig{},
+			expectedConfig: AccessConfig{},
+			expectedErrors: 2,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			errs := test.input.Prepare(&interpolate.Context{}, nil)
+			actualConfig := test.expectedConfig
+
+			if test.expectedErrors > 0 {
+				assert.Len(t, errs.Errors, test.expectedErrors)
+			} else {
+				assert.Nil(t, errs)
+				assert.Equal(t, test.expectedConfig, actualConfig)
+			}
+		})
+	}
+}
+
+func TestDeviceConfig_Prepare(t *testing.T) {
+	type testCase struct {
+		input          *DeviceConfig
+		expectedConfig *DeviceConfig
+	}
+	tests := map[string]testCase{
+		"default values": {
+			input: &DeviceConfig{},
+			expectedConfig: &DeviceConfig{
+				DeviceCPUCores: 2,
+				DeviceMemoryGB: 4,
+				BootDiskSizeGB: 10,
+				SwapDiskSizeGB: 1,
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			errs := test.input.Prepare(&interpolate.Context{}, nil)
+			actualConfig := test.input
+			test.expectedConfig.DeviceName = actualConfig.DeviceName
+
+			assert.Nil(t, errs)
+			assert.Equal(t, test.expectedConfig, actualConfig)
+
+		})
+	}
+}
+
 func TestTemplateConfig_Prepare(t *testing.T) {
 	type testCase struct {
 		input               *TemplateConfig
